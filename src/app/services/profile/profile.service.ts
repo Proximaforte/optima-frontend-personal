22f9910e452eddd58719c9189a5b1f1a79a63c01
Kehinde omosehin <kehindeo@3lineng.com>
2024-07-04 21:45:11 +0100

    fix: crypto url encoded

--- src/app/services/profile/profile.service.ts
+++ src/app/services/profile/profile.service.ts
@@ -27,7 +27,7 @@ export class ProfileService {
     .replace(/\+/g, '-')
     .replace(/\//g, '_')
     .replace(/=+$/, '');
-   let biometricsString:string = `${biometricsUrl}?data=${base64string}`;
+   let biometricsString:string = `${biometricsUrl}?data=${base64String}`;
    window.open(biometricsString, '_blank');
    //window.location.href = biometricsString;
    console.log('url to open>>>',biometricsString);
