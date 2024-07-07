2ed892b663470e5453e00c7de1b7498873daca95
John  Busayo <116476931+JohnBusayo14@users.noreply.github.com>
2024-07-07 17:57:53 +0200

    update-pwa

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from '../src/app/environments/environment.prod';
 
+if (environment.production) {
+  enableProdMode();
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/ngsw-worker.js').then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
