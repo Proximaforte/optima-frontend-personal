35488ebbeaea14ce45e71b4030c35fbb45703335
John  Busayo <116476931+JohnBusayo14@users.noreply.github.com>
2024-07-10 16:43:41 +0200

    icon_update

--- src/app/utilities/navigation/navigation.component.ts
+++ src/app/utilities/navigation/navigation.component.ts
@@ -46,7 +46,7 @@ export class NavigationComponent {
     //   route: '/home/profile',
     // },
     {
-      icon: 'assets/images/Logout.svg',
+      icon: 'assets/images/UserList.svg',
       name: 'logout',
       route: '/auth/login',
     },
