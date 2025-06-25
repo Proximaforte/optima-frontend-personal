8fd2ae790287b74ac73ebfb4fc8293f04b717b7d
tegritechng <112857062+tegritechng@users.noreply.github.com>
2025-06-25 11:20:32 +0100

    Update environment.prod.ts

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
    //baseUrl: 'https://api.optima.com.ng',
-   baseUrl: 'https://chemical-mysimon-british-advisor.trycloudflare.com',
+   baseUrl: 'https://staging-api.optima.com.ng',
    production: true,
     //bashlaw007@gmail.com Password123@
     //email: judeomosehin@gmail.com   password:Password123#
     
 //Tenant Admin:     Email: tosyne.eksu@gmail.com Password: H0l1n355@
-aesKey: 'Q1X8fhzG1HbLJ0BaFvTG9w==',
+  aesKey: 'Q1X8fhzG1HbLJ0BaFvTG9w==',
   aesIv: 'T4kLzo4C7a9ZDwZYqEEm3w=='
 
 }
